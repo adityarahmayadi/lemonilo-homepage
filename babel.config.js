d07b85648409c2d1b7f3069053f8e174affa08bd
aditya rahmayadi <adityarahmayadi@gmail.com>
2020-12-21 22:52:11 +0700

    added dependencies - add react-nativagion package - add babel config module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['babel-plugin-styled-components', {'displayName': false}],
+      ['module-resolver', {
+        root: ['./src'],
+        extensions: ['.js'],
+        alias: {
+          '@lemonilo': './src'
+        }
+      }]
+    ]
   };
 };
