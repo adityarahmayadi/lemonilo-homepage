83edfbae6a6649436ba6ce67367207693c3e4351
aditya rahmayadi <adityarahmayadi@gmail.com>
2020-12-22 08:29:01 +0700

    add bottom tab navigator - add icon bottom tab navigator - add navbar with icon on homescreen

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Box, Images } from '@lemonilo/components'
+import { colors } from '@lemonilo/utils/constants'
 import HomePage from './screens/home'
 import LoginPage from './screens/login'
 
-const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const App = () => (
-  <Stack.Navigator initialRouteName="Home">
-    <Stack.Screen name="Home" component={HomePage} />
-    <Stack.Screen name="Login" component={LoginPage} />
-  </Stack.Navigator>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => {
+        let iconName;
+        if(route.name === 'Beranda'){
+          iconName = focused ? require('@lemonilo/assets/icon/home-active-icon.png') : require('@lemonilo/assets/icon/home-icon.png')
+        } else if (route.name === 'Notifikasi'){
+          iconName = focused ? require('@lemonilo/assets/icon/notifikasi-active-icon.png') : require('@lemonilo/assets/icon/notifikasi-icon.png')
+        } else if (route.name === 'Life'){
+          iconName = focused ? require('@lemonilo/assets/icon/life-active-icon.png') : require('@lemonilo/assets/icon/life-icon.png')
+        } else if (route.name === 'Pesanan'){
+          iconName = focused ? require('@lemonilo/assets/icon/order-active-icon.png') : require('@lemonilo/assets/icon/order-icon.png')
+        } else if (route.name === 'Masuk'){
+          iconName = focused ? require('@lemonilo/assets/icon/profile-active-icon.png') : require('@lemonilo/assets/icon/profile-icon.png')
+        }
+        return (
+          <Box w='20px' h='20px'>
+            <Images w='100%' source={iconName} resizeMode='contain' />
+          </Box>
+        )
+      }
+    })}
+    tabBarOptions={{
+      activeTintColor: colors.green20,
+      inactiveTintColor: 'grey',
+    }}
+  >
+    <Tab.Screen name="Beranda" component={HomePage} />
+    <Tab.Screen name="Notifikasi" component={LoginPage} />
+    <Tab.Screen name="Life" component={LoginPage} />
+    <Tab.Screen name="Pesanan" component={LoginPage} />
+    <Tab.Screen name="Masuk" component={LoginPage} />
+  </Tab.Navigator>
 )
 
 export default App
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { Box, Scroll, Images, Font } from '@lemonilo/components'
+import { Box, Scroll, Images, Font, SafeArea } from '@lemonilo/components'
+import { StatusBar } from 'react-native'
 import Slider from '@lemonilo/components/slider'
 import Categories from '@lemonilo/components/categories'
 import { colors } from '@lemonilo/utils/constants'
@@ -7,25 +8,28 @@ import Shelf from '../components/shelf'
 
 const HomePage = ({ navigation }) => (
   // screen container
-  <Scroll w>
-    {/* slider */}
-    <Slider m='0px 0px 12px' />
+  <SafeArea w f={1} p={`${StatusBar.currentHeight}px 0px 0px 0px`}>
+    <Box p='8px' h='56px' fAlign='center' fDir='row' bg='white'>
+      <Images w='40px' h='40px' source={require('@lemonilo/assets/lemonilo-logo.jpg')} resizeMode='contain' />
+      <Font fontSize='16px' c={colors.green20} >Lemonilo</Font>
+    </Box>
+    <Scroll w>
+      {/* slider */}
+      <Slider m='0px 0px 12px' />
 
-    {/* category icon */}
-    <Categories />
+      {/* category icon */}
+      <Categories />
 
-    {/* shelf - separator */}
-    <Shelf
-      image={require('@lemonilo/assets/separator/separator-1.png')}
-      bg={colors.yellow10}
-      content={
-        <Font fontSize='13px'>Dijamin <Font c={colors.orange10}>Enak, Praktis dan tanpa 3P</Font> (Pengawet, Pewarna dan Perasa Buatan)</Font>
-      }
-    />
-
-    
-
-  </Scroll>
+      {/* shelf - separator */}
+      <Shelf
+        image={require('@lemonilo/assets/separator/separator-1.png')}
+        bg={colors.yellow10}
+        content={
+          <Font fontSize='13px'>Dijamin <Font c={colors.orange10}>Enak, Praktis dan tanpa 3P</Font> (Pengawet, Pewarna dan Perasa Buatan)</Font>
+        }
+      />
+    </Scroll>
+  </SafeArea>
 )
 
 export default HomePage
